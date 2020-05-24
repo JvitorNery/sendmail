9b66c8ac3b9d73265d140111a9ed3a64f5e41c59
jvitornery <jvitornery@bitbucket.com>
2020-05-24 17:10:48 -0300

    first commit

---
+++ controllers/user.js
@@ -0,0 +1,69 @@
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const authMiddleware = require("../middlewares/auth");
+const transporter = require("../middlewares/mail");
+
+const User = mongoose.model("User");
+
+router.post("/register", async (req, res) => {
+  const { email, username } = req.body;
+
+  transporter.sendMail({
+    to: email,
+    from: 'e-mail@gmail.com',
+    subject: 'parabéns você existe!',
+    html: '<h1>email enviado!</h1>'
+  });
+
+  try {
+    if (await User.findOne({ email })) {
+      return res.status(400).json({ error: "Usuário já existe." });
+    }
+
+    const user = await User.create(req.body);
+    
+
+    return res.json({ user });
+  } catch (err) {
+    return res.status(400).json({ error: "Falha ao registrar usuário." });
+  }
+});
+
+router.post("/authenticate", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ error: "Usuário não encontrado." });
+    }
+
+    if (!(await user.compareHash(password))) {
+      return res.status(400).json({ error: "Senha incorreta" });
+    }
+
+    return res.json({
+      user,
+      token: user.generateToken()
+    });
+  } catch (err) {
+    return res.status(400).json({ error: "Falha ao tentar autenticar o usuário." });
+  }
+});
+
+router.use(authMiddleware);
+
+router.get("/me", async (req, res) => {
+  try {
+    const { userId } = req;
+
+    const user = await User.findById(userId);
+
+    return res.json({ user });
+  } catch (err) {
+    return res.status(400).json({ error: "Não conseguimos encontrar dados do usuário" });
+  }
+});
+
+module.exports = router;
